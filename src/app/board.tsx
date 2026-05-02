54725185a11766ef51a1ac9e700ef2c2a53f3d18
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2026-05-01 23:16:55 -0400

    implement cell selections

--- src/app/board.tsx
+++ src/app/board.tsx
@@ -1,23 +1,46 @@
 "use client";
-import { ChessCell, createInitialBoard, getPieceAssetPath } from "@/lib/chess";
+import {
+    ChessCell,
+    Coordinate,
+    createInitialBoard,
+    getPieceAssetPath,
+} from "@/lib/chess";
 import { useState } from "react";
 
 export default function Board() {
     const [board] = useState(() => createInitialBoard());
-    const [selected, setSelected] = useState<string | undefined>(undefined);
+    const [selected, setSelected] = useState<Coordinate | undefined>(undefined);
+
+    const attemptMove = (source: Coordinate, target: Coordinate): boolean => {
+        console.log(
+            `move from \n${JSON.stringify(source)}\n to \n${JSON.stringify(target)}`,
+        );
+
+        return false;
+    };
 
     const cellClick = (cell: ChessCell) => {
-        if (cell.coordinate === selected) setSelected(undefined);
+        // If we don't have any cells selected, just select
+        if (!selected) return setSelected(cell.coordinate);
+
+        // If we select currently selected cell, deselect
+        if (cell.coordinate.equals(selected)) return setSelected(undefined);
+
+        // We have a source and target cell,
+        // let's attempt a move
+        const moveSuccessful = attemptMove(selected, cell.coordinate);
+
+        if (moveSuccessful) setSelected(undefined);
         else setSelected(cell.coordinate);
     };
 
     const cellColor = (cell: ChessCell) => {
         const isSelected = cell.coordinate === selected;
         if (cell.color === "dark") {
-            if (isSelected) return "bg-gray-600";
+            if (isSelected) return "bg-sky-700";
             return "bg-gray-700";
         } else {
-            if (isSelected) return "bg-gray-400";
+            if (isSelected) return "bg-sky-300";
             return "bg-gray-300";
         }
     };
@@ -32,7 +55,7 @@ export default function Board() {
         >
             {board.flat().map((cell) => (
                 <div
-                    key={cell.coordinate}
+                    key={cell.coordinate.key()}
                     className={`flex items-center justify-center ${cellColor(cell)}`}
                     onClick={() => cellClick(cell)}
                 >
